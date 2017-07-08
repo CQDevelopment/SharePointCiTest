6c43e5dcf03fee771109807dda3569c11b7ee5d7
Adrian Johnson <adrian.johnson@cqdev.co.uk>
2017-07-08 01:57:16 +0100

    Building exposure around react components

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+import ReactDOM from 'react-dom'
+
 import Data from './data.js'
 import Test from './components/testComponent.jsx'
 
 export default class App {
 	constructor(props) {
-		this.test = 'test';
+		this.components = {};
+		this.components.testComponent = Test;
+
+		this.render = function(name, target) {
+			ReactDOM.render(this.components[name], document.getElementById(target));
+		};
 	}
 }
